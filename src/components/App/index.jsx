8dc83fc0ad53760fa316b106a9772227862e64eb
Matt Greenlaw <walneergwehttam@googlemail.com>
2019-05-26 11:35:06 -0700

    Clean

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,19 +1,7 @@
 import React, {Fragment} from 'react';
-import {
-  Row,
-  Col,
-  Container,
-} from 'reactstrap'
-import DiceRoller from './../DiceRoller'
-import InitTracker from './../InitTracker'
 
 const App = () => (
   <Fragment >
-    <DiceRoller/>
-    <Container>
-      <InitTracker players={[{name: 'Theron', init: 1},{name: 'Theron', init: 1}]}/>
-    </Container>
-
   </Fragment>
 );
 
