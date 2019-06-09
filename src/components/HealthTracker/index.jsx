5bc9f07e676e98113d640ff13425cfe52206bd08
Matt Greenlaw <walneergwehttam@googlemail.com>
2019-06-09 13:51:01 -0700

    Fix healthtracker tempHP bug

--- src/components/HealthTracker/index.jsx
+++ src/components/HealthTracker/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from "prop-types";
 import {
   Table,
-  Toast,
-  ToastBody,
   Input,
 } from 'reactstrap'
 import './styles.scss'
@@ -17,7 +15,7 @@ export default class HealthTracker extends React.Component {
 
   constructor(props) {
     super(props);
-    this.updateDamage = this.updateDamage.bind(this)
+    this.recieveDamage = this.recieveDamage.bind(this)
     this.state = {
       current: this.props.max,
       temp: 0,
@@ -25,126 +23,142 @@ export default class HealthTracker extends React.Component {
     };
 
     this.props.socket.on("recieveDamage", (damage) => {
+      //Monsters share a common socket,
+      //must further identify by name
       if(this.props.name === damage.name){
           var newDamage = this.state.damage + damage.roll
+          //Limit damage so that at most
+          //current Health = -(maxHP) => damage = maxHP * 2
           if (newDamage > this.props.max * 2)
-            newDamage = this.props.max
-        this.updateDamage(newDamage)
+            newDamage = this.props.max * 2
+        this.recieveDamage(newDamage)
       }
     });
 
     this.props.socket.on("recieveHeal", (healing) => {
+      //Monsters share a common socket,
+      //must further identify by name
       if(this.props.name === healing.name){
         var newDamage = this.state.damage - healing.roll
+        //Cannot have negative damage
         if (newDamage < 0 )
           newDamage = 0
-        this.updateDamage(newDamage)
+        this.recieveDamage(newDamage)
       }
     });
   }
 
-  updateDamage = (damage) => {
+  recieveDamage = (damage) => {
+    //Cannot have negative damage
     if (damage  < 0)
       damage = 0
 
-    var temp = this.state.temp;
+    //Build value to modify max health
+    var modifier = this.state.temp - damage
+    var tempHP
 
-    var modifier = temp - damage
-
-    //var modifier, newTemp
-    //modifier = newTemp = this.state.temp - damage
-    var newTemp
+    //If the modifier represents damage
     if(modifier < 0){
-        newTemp = 0
-        damage = -(modifier)
+      tempHP = 0
+      damage = -(modifier)
     }
+    //else it represents health
     else {
-      newTemp = modifier
+      tempHP = modifier
       damage = 0
     }
 
     this.setState({
       current: +this.props.max + modifier,
-      temp: newTemp,
+      temp: tempHP,
       damage: damage
     })
   }
 
   render(){
-    var temp = this.props.temp;
-    var damage = this.props.damage;
-
+    //Called when munally changing Temp HP
     var setTempHealth = (e) => {
-      if(+e.target.value <= 0){
+      var tempHealth = +e.target.value
+      //Cannot have negative temp health, disreguard anything below 1
+      if(tempHealth <= 0){
         this.setState({
           current: this.props.max - this.state.damage,
           temp: 0
         })
       }
+      //tempHealth > 0, use it to set current
       else{
         this.setState({
-          current: +this.props.max + (+e.target.value) - this.state.damage,
-          temp: +e.target.value
+          current: +this.props.max + tempHealth - this.state.damage,
+          temp: tempHealth
         })
       }
     }
 
+    //Called when manually changing Damage
     var setDamage = (e) => {
       var value = +e.target.value
-      var diff = this.state.damage - value
+      var tempHP = this.state.temp
 
-      if (diff === 1){//removed damaged
-        var newDamage = this.state.damage-1
-        if(newDamage < 0)
-          newDamage = 0
-
-        this.setState({
-          current: +this.props.max + this.state.temp - newDamage,
-          damage: newDamage
-        })
+      //Remove tempHP first
+      //We have tempHP
+      if(tempHP > 0){
+        tempHP = tempHP - value
+        //If we still have tempHP after damage
+        if(tempHP > 0){
+          this.setState({
+            current: this.props.max + tempHP - this.state.damage,
+            temp: tempHP
+          })
+        }
+        //We are out of tempHP after damage
+        else{
+          this.setState({
+            current: this.props.max + tempHP,//tempHP <= 0, so we are adding 0 or a negative number (remaining damage)
+            temp: 0,
+            damage: -(tempHP),
+          })
+        }
       }
-      else{//added dammage
-        this.updateDamage(1)
+      //We don't have tempHP
+      else{
+        this.recieveDamage(value)
       }
     }
 
     return(
-      <Toast fade={false}>
-        <ToastBody>
-          <Table>
-            <thead>
-              <tr>
-                <th>Health</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Current HP</td>
-                <td>{this.state.current + '/' + this.props.max}</td>
-              </tr>
-              <tr>
-                <td>Temp HP</td>
-                <td><Input
-                  onChange={setTempHealth} //@todo validate for negative #s
-                  type="number"
-                  placeholder={this.state.temp}
-                  value={this.state.temp}
-                  className={"width-sm"} /></td>
-              </tr>
-              <tr>
-                <td>Damage</td>
-                <td><Input
-                onChange={setDamage} //@todo validate for negative #s
-                type="number"
-                placeholder={this.state.damage}
-                value={this.state.damage}
-                className={"width-sm"} /></td>
-              </tr>
-            </tbody>
-          </Table>
-        </ToastBody>
-      </Toast>
+      <Table className={"table-dark table-striped"}>
+        <thead>
+          <tr>
+            <th>Health</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Current HP</td>
+            <td>{this.state.current + '/' + this.props.max}</td>
+          </tr>
+          <tr>
+            <td>Temp HP</td>
+            <td><Input
+              onChange={setTempHealth}
+              type="number"
+              placeholder={this.state.temp}
+              value={this.state.temp}
+              className={"width-sm"} /></td>
+          </tr>
+          <tr>
+            <td>Damage</td>
+            <td><Input
+            onChange={setDamage}
+            type="number"
+            placeholder={this.state.damage}
+            value={this.state.damage}
+            className={"width-sm"} /></td>
+          </tr>
+        </tbody>
+      </Table>
     )
   }
 }
